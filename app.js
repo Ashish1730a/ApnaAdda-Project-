1502e69bac766e6214a2344c1058124c2ef39bf4
Ashish1730a <singhashish1730@gmail.com>
2025-10-13 19:52:31 +0530

    add delete button in reviews of listing

--- app.js
+++ app.js
@@ -1,106 +1,111 @@
 // Import required modules
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 // Import the Listing model
-const Listing = require('./models/listing');
-const path =require('path');
-const methodOverride = require('method-override');
-const ejsMate = require('ejs-mate');
+const Listing = require("./models/listing");
+const path = require("path");
+const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
-const ExpressError = require("./utils/ExpressError.js")
-const {listingSchema, reviewSchema}=require("./Schema.js")
-const Reviews = require("./models/review.js")
-
-
-
-
+const ExpressError = require("./utils/ExpressError.js");
+const { listingSchema, reviewSchema } = require("./Schema.js");
+const Reviews = require("./models/review.js");
 
 // Connect to MongoDB database
-main().then(() => {
-    console.log('connect to db');
-}).catch(err => console.log(err));
+main()
+  .then(() => {
+    console.log("connect to db");
+  })
+  .catch((err) => console.log(err));
 
 // Async function to handle database connection
 async function main() {
-    await mongoose.connect('mongodb://127.0.0.1:27017/wonderlust');
+  await mongoose.connect("mongodb://127.0.0.1:27017/wonderlust");
 }
 
-
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(methodOverride('_method'));
-app.engine('ejs', ejsMate);
-app.use(express.static(path.join(__dirname, '/public')));
-
+app.use(methodOverride("_method"));
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "/public")));
 
 // Root route
-app.get('/', (req, res) => {
-    res.send('Hi, I am root');
+app.get("/", (req, res) => {
+  res.send("Hi, I am root");
 });
 
 const validateListing = (req, res, next) => {
-     let {error} = listingSchema.validate(req.body);
-    if(error){
-        throw new ExpressError(400, error)
-    } else {
-        next();
-    }
+  let { error } = listingSchema.validate(req.body);
+  if (error) {
+    throw new ExpressError(400, error);
+  } else {
+    next();
+  }
 };
 
 const validateReview = (req, res, next) => {
-     let {error} = reviewSchema.validate(req.body);
-    if(error){
-        throw new ExpressError(400, error)
-    } else {
-        next();
-    }
+  let { error } = reviewSchema.validate(req.body);
+  if (error) {
+    throw new ExpressError(400, error);
+  } else {
+    next();
+  }
 };
 
 // index route
-app.get('/listings', wrapAsync( async (req, res) => {
-    allListing = await Listing.find({})
-    res.render("listings/index.ejs", {allListing})
-
-}));
-
+app.get(
+  "/listings",
+  wrapAsync(async (req, res) => {
+    allListing = await Listing.find({});
+    res.render("listings/index.ejs", { allListing });
+  })
+);
 
 // new route
 app.get("/listings/new", (req, res) => {
-    res.render("listings/new.ejs");
+  res.render("listings/new.ejs");
 });
 
 // create route
-app.post("/listings", validateListing,  wrapAsync( async (req, res, next) => {
-   
-
+app.post(
+  "/listings",
+  validateListing,
+  wrapAsync(async (req, res, next) => {
     const newListing = new Listing(req.body.listing);
     await newListing.save();
     res.render("listings/show.ejs", { listing: newListing });
-    
-}));
+  })
+);
 
 // show route
-app.get("/listings/:id",wrapAsync( async (req, res) => {
-    let {id} = req.params;
-    const listing =await Listing.findById(id).populate("reviews");
-    res.render("listings/show.ejs", {listing});
-}))
-
+app.get(
+  "/listings/:id",
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id).populate("reviews");
+    res.render("listings/show.ejs", { listing });
+  })
+);
 
 // Edit route
-app.get("/listings/:id/edit",wrapAsync( async (req, res) => {
-    let {id} = req.params;
-    const listing =await Listing.findById(id);
-    res.render("listings/edit.ejs", {listing});
-}));
-
+app.get(
+  "/listings/:id/edit",
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/edit.ejs", { listing });
+  })
+);
 
 // Update route
 // Update route
-app.put("/listings/:id", validateListing, wrapAsync( async (req, res) => {
+app.put(
+  "/listings/:id",
+  validateListing,
+  wrapAsync(async (req, res) => {
     let { id } = req.params;
 
     // Normal fields update
@@ -108,41 +113,59 @@ app.put("/listings/:id", validateListing, wrapAsync( async (req, res) => {
 
     // Agar user ne image URL diya hai to usse object banake save karo
     if (updatedData.image) {
-        updatedData.image = { 
-            filename: "listingimage", 
-            url: updatedData.image 
-        };
+      updatedData.image = {
+        filename: "listingimage",
+        url: updatedData.image,
+      };
     }
 
     await Listing.findByIdAndUpdate(id, updatedData, { runValidators: true });
 
     res.redirect(`/listings/${id}`);
-}));
-
-
+  })
+);
 
 // Delete route
-app.delete("/listings/:id",wrapAsync( async (req, res) => {
-    let {id} = req.params;
+app.delete(
+  "/listings/:id",
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
     let deletedListing = await Listing.findByIdAndDelete(id);
     console.log(deletedListing);
     res.redirect("/listings");
-}));
+  })
+);
 
 // Reviews
 // Post Route
-app.post("/listings/:id/reviews", validateReview , wrapAsync( async(req, res) => {
-   let listing = await Listing.findById(req.params.id);
-   let newReview = new Reviews(req.body.review);
+app.post(
+  "/listings/:id/reviews",
+  validateReview,
+  wrapAsync(async (req, res) => {
+    let listing = await Listing.findById(req.params.id);
+    let newReview = new Reviews(req.body.review);
 
-   listing.reviews.push(newReview);
+    listing.reviews.push(newReview);
 
-   await newReview.save();
-   await listing.save();
+    await newReview.save();
+    await listing.save();
 
-   res.redirect(`/listings/${listing._id}`);
-}))
+    res.redirect(`/listings/${listing._id}`);
+  })
+);
 
+// Delete Review Route
+app.delete(
+  "/listings/:id/reviews/:reviewId",
+  wrapAsync(async (req, res) => {
+    let { id, reviewId } = req.params;
+
+    await Listing.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+    await Reviews.findByIdAndDelete(reviewId);
+
+    res.redirect(`/listings/${id}`);
+  })
+);
 
 // Route to test inserting a sample listing into the database
 // app.get("/testListing", async(req,res) => {
@@ -157,7 +180,7 @@ app.post("/listings/:id/reviews", validateReview , wrapAsync( async(req, res) =>
 //     // Save the document to MongoDB
 //     await sampleListing.save();
 //     console.log("Sample was saved");
-//     res.send("success testing");    
+//     res.send("success testing");
 // })
 
 app.use((req, res, next) => {
@@ -165,12 +188,12 @@ app.use((req, res, next) => {
 });
 
 app.use((err, req, res, next) => {
-    let {statusCode=500, message="Something went wrong !"} = err;
-    res.status(statusCode).render("error.ejs", {message});
-    // res.status(statusCode).send(message);
-})
+  let { statusCode = 500, message = "Something went wrong !" } = err;
+  res.status(statusCode).render("error.ejs", { message });
+  // res.status(statusCode).send(message);
+});
 
 // Start the Express server
 app.listen(8080, () => {
-    console.log('Server is running on port 8080');
-})
+  console.log("Server is running on port 8080");
+});
